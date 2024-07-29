d4cb88117dfa4f2c6ebed2f97ddf6c76a2754ff8
Anish-Hs <Anish-Hs@github.com>
2024-07-29 15:43:46 +0200

    Updated and checked the random number unit test

--- tests/Test_return_random_number.py
+++ tests/Test_return_random_number.py
@@ -2,6 +2,6 @@ from src.utils import return_random_number
 
 try:
   return_random_number(0,200, 45)
-  print("The reurtn_random_number function works")
+  print("The return_random_number function works")
 except:
   print("We have an issue with return_random_number function")
